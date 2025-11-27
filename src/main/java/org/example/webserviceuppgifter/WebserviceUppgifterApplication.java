4cdd30987437ce06c09f99a1f3359539bc393893
Erik.Knoos <erik.knoos@stud.sti.se>
2025-11-27 16:09:31 +0100

    Initial commit

---
+++ src/main/java/org/example/webserviceuppgifter/WebserviceUppgifterApplication.java
@@ -0,0 +1,13 @@
+package org.example.webserviceuppgifter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceUppgifterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebserviceUppgifterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/webserviceuppgifter/WebserviceUppgifterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webserviceuppgifter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceUppgifterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
